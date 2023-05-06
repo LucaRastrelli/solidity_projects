40b51321a013e164f6f508333ab665b6ef133952
Luca Rastrelli <rastrelli.luca@outlook.it>
2023-05-06 11:58:24 +0200

    Added second test

--- pet_shop/test/test_adoption.js
+++ pet_shop/test/test_adoption.js
@@ -21,5 +21,10 @@ contract("Adoption", function (accounts) {
       const adopter = await adoption.adopters(8);
       assert.equal(adopter, expectedAdopter, "The owner of the adopted should be the first account");
     });
+
+    it ("Can fetch the collection of all pet owner's addresses", async () => {
+      const adopters = await adoption.getAdopters();
+      assert.equal(adopters[8], expectedAdopter, "The owner of the adopted per should be in the collection.");
+    });
   })
 });
