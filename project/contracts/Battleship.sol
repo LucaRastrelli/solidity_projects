4043f40d380ae357b37a2147028b39c0d3b992fc
Luca Rastrelli <rastrelli.luca@outlook.it>
2023-05-17 12:00:23 +0200

    new files and function

--- project/contracts/Battleship.sol
+++ project/contracts/Battleship.sol
@@ -4,4 +4,16 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Battleship {
   constructor() public {
   }
+
+  function newGame() {}
+
+  function joinGame() {} //gestisce sia il random che non
+
+  function newMove() {} //gestisce nuova mossa
+
+  function checkWinner() {}
+
+  function endGame() {}
+
+  function afkPlayer() {}
 }
