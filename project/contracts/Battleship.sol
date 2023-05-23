eb6f5b2991f0d779e85d07b081d84a3494b307c3
Luca Rastrelli <rastrelli.luca@outlook.it>
2023-05-23 22:13:20 +0200

    newgame, joingame, bet

--- project/contracts/Battleship.sol
+++ project/contracts/Battleship.sol
@@ -2,32 +2,113 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Battleship {
-  uint8 constant GRID_SIZE = 10;
+  //uint8 constant GRID_SIZE = 10;
   uint256 private nextGameID = 1;
+  uint256 private counter = 0;  //conta il numero di partite aperte
 
   struct Game {
+    address player;
+    address enemy;    //address(0): non c'è il giocatore
     bytes32 playerGridHash;
     bytes32 enemyGridHash;
-    bool open;        //=false ci sono due giocatori; =true c'è un giocatore
-    bool ended;       //=false la partita non è finita; =true la partita è finita
+    bool ended;       //false: la partita non è finita; true: la partita è finita
+    uint8 playerOffer;
+    uint8 enemyOffer;
   }
 
   mapping (uint256 => Game) private games;
-  event NewGameCreated(uint256 idGame);
+  event NewGameCreated(address player, uint256 idGame);
+  event JoinedGame(address player, address enemy, uint256 idGame);
+  event BoardAdded();
+  event OfferReceived(address bidder, uint8 offer, uint256 idGame);
+  event CommonOffer(uint256 idGame);
 
   function newGame() public {
-    emit NewGameCreated(nextGameID);
-    games[nextGameID].open = true;
+    emit NewGameCreated(msg.sender, nextGameID);
+    games[nextGameID].player = msg.sender;
+    games[nextGameID].enemy = address(0);
+    games[nextGameID].ended = false;
+    games[nextGameID].playerOffer = 0;
+    games[nextGameID].enemyOffer = 0;
+    games[nextGameID].playerGridHash = 0;
+    games[nextGameID].enemyGridHash = 0;
     nextGameID++;
+    counter++;
   }
 
-  function joinGame(uint gameId) public {} //gestisce sia il random che non
+  function joinGame(uint256 gameId) public {   //gestisce sia il random che non
+    require(gameId >= 0, "ID must be greater than 0");
+    require(gameId < nextGameID, "Game not open");
+    require(counter > 0, "No games available");
 
-  function attack() public{}
+    //randomness
+    if(gameId == 0) {
+      bytes32 bhash = blockhash(block.number - 1);
+      bytes memory bytesArray = new bytes(32);
+      for (uint i; i < 32; i++) {
+          bytesArray[i] = bhash[i];
+      }
+      bytes32 rand = keccak256(bytesArray);
+      uint256 index = uint256(rand) % counter + 1;
+
+      for(uint256 j = 1; j < nextGameID; j++) {
+        if(index == 0) {
+          break;
+        }
+        if(games[j].enemy == address(0)) {
+          index--;
+          if(games[j].player != msg.sender)   //un giocatore non può unirsi alla sua stessa partita
+            gameId = j;
+        }
+      }
+      if(gameId == 0)
+        revert("Match not found");
+    }
+    //not random
+    else {
+      if(games[gameId].player == msg.sender)
+        revert("You cannot join your own game"); 
+      if(games[gameId].enemy != address(0))
+        revert("Game already has two players");      
+    }
+    games[gameId].enemy = msg.sender;
+    counter--;
+    emit JoinedGame(games[gameId].player, msg.sender, gameId);
+  } 
+
+  //function ricevisoldi?
+
+  function bet(uint256 gameId, uint8 offer) public {
+    require(gameId >= 0, "ID must be greater than 0");
+    require(gameId < nextGameID, "Game not open");
+    if (games[gameId].playerOffer == games[gameId].enemyOffer) require(games[gameId].playerOffer == 0);
+    else require(games[gameId].playerOffer != games[gameId].enemyOffer);
+
+    if (games[gameId].player == msg.sender) games[gameId].playerOffer = offer;
+    if (games[gameId].enemy == msg.sender) games[gameId].enemyOffer = offer;
+
+    emit OfferReceived(msg.sender, offer, gameId);
+
+    if (games[gameId].playerOffer == games[gameId].enemyOffer) {
+      emit CommonOffer(gameId);
+    }
+
+  }
+
+  function attack() public {}
 
   function checkWinner() private {}
 
   function endGame() private {}
 
   function afkPlayer() public {}
+
+  function board(bytes32 boardHash, uint256 gameID) public {
+    require(gameID < nextGameID);
+    require(gameID > 0);
+
+    emit BoardAdded();
+    games[gameID].playerGridHash = boardHash;
+  }
+
 }
--- project/src/js/app.js
+++ project/src/js/app.js
@@ -1,21 +1,15 @@
+var waitingForID = false;
+var waitingForJoin = false;
+var waitingForSync = false;
+var IDGame = 0;
+var ethOffer = 0;
+
 App = {
   web3Provider: null,
   contracts: {},
+  account: "0x0",
 
   init: async function() {
-    // Load ships.
-    $.getJSON('../ships.json', function(data) {
-      var shipsRow = $('#shipsRow');
-      var shipTemplate = $('#shipTemplate');
-
-      for (i = 0; i < data.length; i ++) {
-        shipTemplate.find('.panel-title').text(data[i].name);
-        shipTemplate.find('.ship-width').text(data[i].width);
-
-        shipsRow.append(shipTemplate.html());
-      }
-    });
-
     return await App.initWeb3();
   },
 
@@ -39,49 +33,33 @@ App = {
     return App.initContract();
   },
 
-  newGame: function(event) {
-    event.preventDefault();
+  initContract: function() {
+    web3.eth.getCoinbase(function(err, account) {
+      if(err == null) {
+          App.account = account;
+          $("#accountId").html(account);
+      }
+    });
+    $.getJSON("Battleship.json", function(data) {
+      var BattleshipArtifact = data;    //Get the contract artifact and initialize it
+      App.contracts.Battleship = TruffleContract(BattleshipArtifact);
+      App.contracts.Battleship.setProvider(App.web3Provider);
 
+      return App.listenForEvents();
+    });
+    return App.bindEvents();
+  },
+
+  newGame: function() {
     console.log("newGame");
-    var gameID;
     var battleshipInstance;
-    web3.eth.getAccounts(function(error, accounts) {
-      if(error) { console.log(error);}
-      var account = accounts[0];
-
-      web3.eth.getCoinbase(function(error, coinbase) {
-        if (error) {
-          console.log(error);
-        }
-        if (coinbase) {
-          account = coinbase; // Utilizza l'indirizzo dell'utente effettivo come account
-        }
-      });
-
-      console.log(account)
-      App.contracts.Battleship.deployed().then(function (instance) {
-        battleshipInstance = instance;
-        return battleshipInstance.newGame({from: account});
-      }).then(function () {
-        
-        battleshipInstance.NewGameCreated(function (err, result) {
-          if(err) {
-            return error(err);
-          }
-          var gameIDNumber = result.args.idGame.toNumber();
-          if(gameIDNumber > 0) {
-            gameID = gameIDNumber;
-            console.log(gameID);
-
-            $('.splash-container').find('button').remove();
-            $('.splash-container').find('table').remove();
-            $('.splash-container').find('input').remove();
-            $('.splash-container').append(gameID);
-          }
-        })
-      }).catch(function(err) {
-        console.log(err.message);
-      });
+    console.log(App.account)
+    App.contracts.Battleship.deployed().then(function (instance) {
+      battleshipInstance = instance;
+      waitingForID = true;
+      return battleshipInstance.newGame({from: App.account});
+    }).catch(function(err) {
+      console.error(err.message);
     });
   },
 
@@ -94,19 +72,110 @@ App = {
     console.log("joinGame");
     //0: random
     var gameID = document.getElementById("gameID").value;
+    var battleshipInstance;
+    if(gameID < 0) gameID = 0;
     console.log(gameID);
+    console.log(App.account);
+    App.contracts.Battleship.deployed().then(function (instance) {
+      battleshipInstance = instance;
+      waitingForJoin = true;
+      return battleshipInstance.joinGame(gameID, {from: App.account});
+    }).catch(function(err) {
+      console.error(err.message);
+    });
   },
 
-  initContract: function() {
-    $.getJSON("Battleship.json", function(data) {
-      var BattleshipArtifact = data;    //Get the contract artifact and initialize it
-      App.contracts.Battleship = TruffleContract(BattleshipArtifact);
-
-      App.contracts.Battleship.setProvider(App.web3Provider);
-      
-      return;
+  bet: function() {
+    var offer = document.getElementById("bet").value;
+    console.log(offer);
+    if (offer <= 0) offer = 1;
+    App.contracts.Battleship.deployed().then(function (instance) {
+      return instance.bet(IDGame, offer, {from: App.account});
+    }).catch(function (err) {
+      console.error(err.message);
     });
-    return App.bindEvents();
+
+  },
+
+  listenForEvents: function() {
+    App.contracts.Battleship.deployed().then(function (instance) {
+      instance.NewGameCreated(function (err, result) {
+        if(waitingForID == false) return;
+        if(result.args.player != App.account) return;
+        if(err) {
+          console.error(err);
+        }
+        waitingForID = false;
+        IDGame = result.args.idGame.toNumber();
+        console.log(IDGame);
+        $('.splash-container').find('button').remove();
+        $('.splash-container').find('input').remove();
+        $('.splash-container').find('button').remove();
+
+        $('.splash-container').append("<div class='waiting'</div>");
+        $('.waiting').append("Game ID: ");
+        $('.waiting').append(result.args.idGame.toNumber());
+        $('.waiting').append('<br>Waiting for player...');
+        waitingForJoin = true;
+          
+      });
+
+      instance.JoinedGame(function (err, result) {
+        if(err) {
+          console.error(err);
+        }
+        if(waitingForJoin == false) return;
+        if(result.args.enemy == App.account) {
+          IDGame = result.args.idGame.toNumber();
+        }
+        else if(result.args.player == App.account ) {
+          if(result.args.idGame.toNumber() != IDGame) return;
+        }
+        else return;
+        waitingForJoin = false;
+        waitingForSync = true;
+        $('.splash-container').find('button').remove();
+        $('.splash-container').find('input').remove();
+        $('.waiting').remove();
+        
+        $('.splash-container').append("Game ID ");
+        $('.splash-container').append(IDGame);
+        $('.splash-container').append(" is starting. Choose how many ETH you want to bet: ");
+        $('.splash-container').append('<input type="number" id="bet" placeholder="ETH" min="1" value="1">');
+        $('.splash-container').append('<br></br>');
+        $('.splash-container').append('<button class="btn splash-btn" id="bet-btn">Bet</button>');
+        $(document).on('click', '#bet-btn', App.bet);
+
+        $('.splash-container').append('<br></br>');
+        $('.splash-container').append('<div class="decision"</div>');
+        $('.decision').append('<div class="offer"</div>');
+
+        $('.offer').append('Your adversary is still thinking...')
+
+      });
+      
+      instance.OfferReceived(function (err, result) {
+        if(err) {
+          console.error(err);
+        }
+        if(waitingForSync == false) return;
+        if(result.args.idGame.toNumber() != IDGame) return;
+        if(result.args.bidder == App.account) return; //ignora l'event se ho fatto io la proposta
+        
+        ethOffer = result.args.offer.toNumber();
+
+        $('.offer').remove();
+        $('.decision').append('<div class="offer"</div>');
+        $('.offer').append('Your adversary propose: ');
+        $('.offer').append(ethOffer);
+        $('.offer').append(' ETH');
+      });
+
+      instance.CommonOffer(function (err, result) {
+
+      });
+
+    });
   }
 };
 
